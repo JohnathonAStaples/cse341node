4f4f7f2a69aeabaf8ee11712907cbf64990a91a8
Johnathon Staples <staplesjohnathona@gmail.com>
2024-03-02 13:31:10 -0700

    going above and beyond lesson 1

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const johnathonRoute = (req, res) => {
+    res.send('Johnathon Staples')
+};
+
+const angelaRoute = (req, res) => {
+    res.send('Angela Staples')
+};
+
+module.exports = {
+    johnathonRoute,
+    angelaRoute
+};
