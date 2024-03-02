fa866fbe4882fe3994b0d9de917c890a7e766b40
Johnathon Staples <staplesjohnathona@gmail.com>
2024-03-02 11:53:41 -0700

    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Angela Staples');
+    res.send('Johnathon Staples');
 });
 
 app.listen(process.env.port || 3000);
