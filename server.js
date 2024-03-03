5eb631a14eb650568963cca5babae42d41c04ce6
Johnathon Staples <staplesjohnathona@gmail.com>
2024-03-02 20:34:20 -0700

    mongo

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(process.env.port || port);
-console.log('Web Server is listening at port ' + (process.env.poirt || port));
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err)
+    }
+    else {
+        app.listen(process.env.port || port);
+        console.log(' Database & Web Server is listening at port ' + (process.env.poirt || port));
+    }
+})
+
+// app.listen(process.env.port || port);
+// console.log('Web Server is listening at port ' + (process.env.poirt || port));
